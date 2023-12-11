84e504e28253a9836462b609e267106a11c0b6d4
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-11 18:50:19 -0500

    Adds menu view UI

--- Restaurant/MenuView.swift
+++ Restaurant/MenuView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct MenuView: View {
+    @State var menuItems: [MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ForEach(menuItems) { item in
+                MenuListRow(item: item)
+            }
+        }
+        .onAppear {
+            menuItems = dataService.getData()
+        }
     }
 }
+
 
 #Preview {
     MenuView()
