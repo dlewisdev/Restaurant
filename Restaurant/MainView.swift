433aa31ae712519c3b8b64d78789bfaa344ec7a4
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-11 18:50:09 -0500

    Adds a tab view to the mainview UI

--- Restaurant/MainView.swift
+++ Restaurant/MainView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            MenuView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "menucard")
+                        Text("Menu")
+                    }
+                }
+            
+            AboutView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "info.circle")
+                        Text("About")
+                    }
+                }
+            
+            GalleryView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "photo")
+                        Text("Gellery")
+                    }
+                }
+        }
     }
 }
 
