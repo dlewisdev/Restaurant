c9f5acd7f254faa0db660a770511758b2c8637f3
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-11 17:16:00 -0500

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Danielle Lewis on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
