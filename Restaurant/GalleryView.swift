b5345c7cb76a910ea83a0eff2da022f5f7d1e402
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-11 18:49:54 -0500

    Adds galleryview UI

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct GalleryView: View {
+    @State var photoData = [String]()
+    var dataService = DataService()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Gallery")
+                .font(.largeTitle)
+                .bold()
+            
+            GeometryReader { geo in
+                ScrollView(showsIndicators: false) {
+                    LazyVGrid(columns: [GridItem(spacing: 10),
+                                        GridItem(spacing: 10),
+                                        GridItem(spacing: 10)],
+                              spacing: 10) {
+                        ForEach(photoData, id: \.self) { photo in
+                            Image(photo)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: (geo.size.width - 50) / 3)
+                                .clipped()
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                .onAppear {
+                    photoData = dataService.getGalleryData()
+                }
+            }
+        }
     }
 }
 
